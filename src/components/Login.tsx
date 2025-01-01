8201583e4cb0cd117657f814f60f75cfdb5019a0
imkrishnasarathi <krishnasarathishem@gmail.com>
2025-01-01 11:58:10 +0530

    create a login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import {account } from "../appwrite";
+
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await account.createSession(email, password);
+            alert('Logged in successfully');
+        } catch (error) {
+            alert('Failed to login');
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default Login;

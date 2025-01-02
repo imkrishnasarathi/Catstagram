074a5ec847eccc92daf3228b4cc7dc0ca1fbe945
imkrishnasarathi <krishnasarathishem@gmail.com>
2025-01-02 12:52:44 +0530

    fix authentication issues with appwrite

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -4,7 +4,7 @@ const client = new Client();
 const account = new Account(client);
 
 client
-    .setEndpoint('http://localhost/v1') 
+    .setEndpoint('https://cloud.appwrite.io/v1') 
     .setProject('6774db4f0019e0f9e984')
 
 export { client, account };
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
 import { account } from "../appwrite";
+import { ID } from "appwrite";
 
 function SignUp(){
-    const [userId, setUserId] = useState('');
+    const [userName, setUserId] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
-            await account.create(userId, email, password);
+            await account.create(ID.unique(), email, password, userName);
             alert('Account created successfully');
         } catch (error) {
             alert('Failed to create account');
+            console.log(error);
         }
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="Username" />
+            <input type="text" value={userName} onChange={(e) => setUserId(e.target.value)} placeholder="Username" />
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             <button type="submit">Sign Up</button>

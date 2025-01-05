a3e3203437771704032ca0fd6db30c17ba02eb3c
imkrishnasarathi <krishnasarathishem@gmail.com>
2025-01-05 18:12:26 +0530

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,29 @@
 import Navbar from './Navbar';
 import FooterBar from './footerBar';
 import PostSection from './postSection';
+import { styled } from '@mui/material';
 
-function Home(){
-    return(
-        <>
-            <Navbar/>
-            <PostSection/>
-            <FooterBar/>
-        </>
-    )
+const PageContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+}));
+
+const ContentWrapper = styled('div')(({ theme }) => ({
+    flex: 1, 
+    paddingBottom: theme.spacing(2), 
+}));
+
+function Home() {
+    return (
+        <PageContainer>
+            <Navbar />
+            <ContentWrapper>
+                <PostSection />
+            </ContentWrapper>
+            <FooterBar />
+        </PageContainer>
+    );
 }
 
 export default Home;

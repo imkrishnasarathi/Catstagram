678a39d7decba2fdf37709e16a4efa1a21961482
krishnasarathi <krishnasarathishem@gmail.com>
2025-01-10 20:33:41 +0530

    update login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -18,8 +18,10 @@ function Login(){
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <label htmlFor="email">Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
+            <label htmlFor="password">Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
             <button type="submit">Login</button>
         </form>
     );

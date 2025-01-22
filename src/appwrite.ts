a5899341bb0317d093b151f4ea4512e90837a56a
Krishna Sarathi Ghosh <85837080+imkrishnasarathi@users.noreply.github.com>
2025-01-22 21:14:12 +0530

    Update appwrite.ts

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -3,9 +3,7 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 const client = new Client();
 const account = new Account(client);
 
-client
-.setEndpoint('https://cloud.appwrite.io/v1') 
-.setProject('6774db4f0019e0f9e984')
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('6774db4f0019e0f9e984')
 
 const databases = new Databases(client);
 const storage = new Storage(client);

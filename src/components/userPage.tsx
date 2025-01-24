3bb1f0cc6a70b6b2b889b194cf619a18b0a4ffa2
Krishna Sarathi Ghosh <85837080+imkrishnasarathi@users.noreply.github.com>
2025-01-24 18:51:21 +0530

    Update userPage.tsx

--- src/components/userPage.tsx
+++ src/components/userPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './userPage.module.css';
 import { account } from '../appwrite';
